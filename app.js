53f75a178ea8ce5c6b3278e8b3db227c63afa92a
Izzy Edson <isabelle.edson@hyatt.com>
2019-11-26 14:04:29 -0600

    all tests passed

---
+++ app.js
@@ -0,0 +1,39 @@
+//Global Variables 
+
+//pull team info from JS files 
+
+//write team profile HTML based on user inputs
+    //Employee inputs 
+        //Name
+        //ID
+        //Email
+        // Role - Choices Intern, Engineer, Manager
+        //then
+
+        //if intern create new Intern
+
+        //if engineer create new Engineer
+
+        //if manager create new Manager
+
+        //then push answers to object
+
+    //new Intern Create Functions
+        // Prompt ask what school they went to School
+        //then push to object
+
+    //new Engineer Create Function
+        //Prompt ask what their github username is 
+        //then push to oject
+    //new Manager Create Function 
+        //Prompt what is their office nubmer
+        //then push to object
+
+    //Create HTML 
+        //populate HTML Cards with Employee Info
+        //Jumbotron Team Roster
+        //Manager Stats
+        //Engineer Stats
+        //Intern Stats
+        //footer Stats
+
